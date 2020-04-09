4c44a6a74a03ade03750052ce2ca72a3e0b57b2f
keis-one <keis.one.work@gmail.com>
2020-04-09 10:46:49 +0900

    プロジェクト新規作成

---
+++ Basic01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Basic01Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Basic01Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
